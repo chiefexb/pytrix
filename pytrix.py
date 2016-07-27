8d05d005987c8b0d04d02f5f7ccf76ed10edfb62
chiefexb <chief_exb@mail.ru>
2016-07-28 02:33:29 +0300

    2nd

--- pytrix.py
+++ pytrix.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 import pygame
+class Block(pygame.sprite.Sprite):
+
+    # Constructor. Pass in the color of the block,
+    # and its x and y position
+    def __init__(self, color, width, height):
+       # Call the parent class (Sprite) constructor
+       pygame.sprite.Sprite.__init__(self)
+
+       # Create an image of the block, and fill it with a color.
+       # This could also be an image loaded from the disk.
+       self.image = pygame.Surface([width, height])
+       self.image.fill(color)
+
+       # Fetch the rectangle object that has the dimensions of the image
+       # Update the position of this object by setting the values of rect.x and rect.y
+       self.rect = self.image.get_rect()
 def main():
     size = (700, 500)
     pygame.init()
@@ -8,6 +24,11 @@ def main():
     done = False
     clock = pygame.time.Clock()
     WHITE = (255, 255, 255)
+    black=(50,50,50)
+    bl=    (0,1,0,
+           0,1,1,
+           0,0,1)
+    B=Block
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
